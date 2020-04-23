261761fd5b433928280592972b455b8a70835679
JuniorTecsup <54334297+JuniorTecsup@users.noreply.github.com>
2020-04-23 12:01:38 -0500

    make it better

--- index.php
+++ index.php
@@ -1,19 +1,68 @@
-<?php
-include("auth.php");
-?>
-<!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<title>Inicio</title>
-<link rel="stylesheet" href="style.css" />
-</head>
-<body>
-<div class="form">
-<h1 >Bienvenido <?php echo $_SESSION['username']; ?>!</h1>
-<p >Informate aqui de las ultimas noticias!</p>
-<p><a href="dashboard.php">Ver noticias</a></p>
-<a href="logout.php">Cerrar sesion</a>
-</div>
-</body>
+    <head>
+
+    </head>
+    <body>
+        <?php
+            require('process.php');
+
+            $db_con = new MySqlDrive();
+            $query = "SELECT * FROM student";
+            // $result = $db_con->read($query);
+            $result = mysqli_query($db_con->db_connect(), $query);
+            // var_dump($result);
+        ?>
+
+        <?php if(isset($_SESSION['message'])): ?>
+
+        <div>
+            <h3>
+                <?php echo $_SESSION['message']; ?>
+            </h3>
+        </div>
+
+        <?php endif ?>
+
+        <div class="row">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Phone</th>
+                        <th>Roll</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while($row = $result->fetch_assoc()){ ?>
+                    <tr>
+                        <td><?php echo $row['name']; ?></td>
+                        <td><?php echo $row['phone']; ?></td>
+                        <td><?php echo $row['roll']; ?></td>
+                        <td>
+                            <a href="index.php?edit=<?php echo $row['id']?>">Edit</a>
+                            <a href="process.php?delete=<?php echo $row['id']?>">Delete</a>
+                        </td>
+                    </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+
+        </div>
+        <br>
+        <form action="process.php" method="POST">
+            <label for="">Name</label>
+            <input type="text" value="<?php echo $name;?>" name="name">
+            <label for="">Phone</label>
+            <input type="text" value="<?php echo $phone;?>" name="phone">
+            <label for="">Roll</label>
+            <input type="number" value="<?php echo $roll;?>" name="roll">
+            <input type="hidden" value="<?php echo $id;?>" name="id">
+            <?php if($update == true): ; ?>
+                <button type="submit" name="update">Update</button>
+            <?php else: ?>
+                <button type="submit" name="save">Submit</button>
+            <?php endif ;?>
+        </form>
+    </body>
 </html>
