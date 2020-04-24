ce1c5d0cc2df652276363692ff223c673e6ddbf7
JuniorTecsup <54334297+JuniorTecsup@users.noreply.github.com>
2020-04-24 03:15:54 -0500

    v4

--- empleados.php
+++ empleados.php
@@ -5,8 +5,8 @@ include("auth.php");//estas logeado?
     <head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+
     </head>
     <body>
         <?php
@@ -33,8 +33,8 @@ include("auth.php");//estas logeado?
         </ul>
 </div>
 
-        <div class="container" style="margin-left: 3px;">
-        <div class="table">
+        <div class="container" style="margin-left: 3px;font-size: 16px;">
+        <div class="table" id="Datos">
             <table class="table table-bordered">
                 <thead>
                     <tr>
@@ -78,5 +78,24 @@ include("auth.php");//estas logeado?
         </form>
     </div>
 </div>
-    </body>
+
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+ <script type="text/javascript" src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+ <script type="text/javascript">
+ 
+    $(document).ready( function () {
+      
+      $('#Datos').DataTable({
+    "language": {
+      "url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
+    },"lengthMenu": [ 2, 5, 10 ]
+  });
+    }); 
+
+ </script>
+
+</body>
 </html>
